0763687d88bab8c3b490ca2573d90cec37522283
bill donner <billdonner@gmail.com>
2024-07-05 12:38:25 -0500

    Initial Commit

---
+++ calert4/calert4App.swift
@@ -0,0 +1,17 @@
+//
+//  calert4App.swift
+//  calert4
+//
+//  Created by bill donner on 7/5/24.
+//
+
+import SwiftUI
+
+@main
+struct calert4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
